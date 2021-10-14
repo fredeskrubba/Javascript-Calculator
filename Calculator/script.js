e5bc4f3db48ea0be4e153ceb4061d1f4dadf532a
Frederik Skrubbeltrang <80112882+fredeskrubba@users.noreply.github.com>
2021-10-14 09:44:50 +0200

    Add files via upload

--- Calculator/script.js
+++ Calculator/script.js
@@ -29,38 +29,39 @@ function operate(operator, opNum1, opNum2){
     }
 };
 
-// create the functions that populate the display
-
 // Object to keep track of all the information passed
 const calculatorInfo = {
-    inputNumber1: 0,
+    inputNumber1: "",
     inputOperator: "",
-    inputNumber2: 0,
+    inputNumber2: "",
     currentSum: 0,
 };
 
 let displayNum = document.querySelector(".display");
 
-let numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let operatorArray = ["+", "-", "*", "/"];
+const numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const operatorArray = ["+", "-", "*", "/"];
 
+// create the functions that populate the display
 // Check if the button is a number or an operator, by checking if it's in the former arrays.
 // if it is, update the object first, then update the display.
 function changeDisplay(newNum){
     if (numberArray.includes(parseInt(newNum)) && operatorArray.includes(calculatorInfo.inputOperator)){
-        calculatorInfo.inputNumber2 = parseInt(newNum);
+        calculatorInfo.inputNumber2 += newNum;
+        displayNum.textContent = calculatorInfo.inputNumber2;
     } else if (numberArray.includes(parseInt(newNum))){
-        calculatorInfo.inputNumber1 = parseInt(newNum);
+        calculatorInfo.inputNumber1 += newNum;
+        displayNum.textContent = calculatorInfo.inputNumber1;
+        
     } else if (operatorArray.includes(newNum)){
         calculatorInfo.inputOperator = newNum;
+        displayNum.textContent = calculatorInfo.inputOperator;
     };
-    return displayNum.textContent = newNum;
 };
 
 // make a nodelist of every button with queryselectorall and convert it to an array.
-// buttons with numbers
-let buttonList = document.body.querySelectorAll("button");
-let buttonArray = Array.from(buttonList);
+const buttonList = document.body.querySelectorAll("button");
+const buttonArray = Array.from(buttonList);
 
 
 // A foreach loop that add a click event to every element in the buttonArray array, and calls the changeDisplay function. 
@@ -73,17 +74,18 @@ buttonArray.forEach(button => {
 
 // A function that calls the operate function when the = button is pressed.
 function calculate(){
-    calculatorInfo.currentSum = operate(calculatorInfo.inputOperator, calculatorInfo.inputNumber1, calculatorInfo.inputNumber2);
+    calculatorInfo.currentSum = operate(calculatorInfo.inputOperator, parseInt(calculatorInfo.inputNumber1), parseInt(calculatorInfo.inputNumber2));
     return displayNum.textContent = calculatorInfo.currentSum;
 };
 
 // function that resets the calculatorInfo object so you can do math again.
 function reset(){
-    calculatorInfo.inputNumber1 = 0;
-    calculatorInfo.inputNumber2 = 0;
+    calculatorInfo.inputNumber1 = "";
+    calculatorInfo.inputNumber2 = "";
     calculatorInfo.inputOperator = "";
 };
 
+// Use the info from the calculatorInfo object to do math when = is pressed, then reset the info. 
 const equals = document.querySelector(".equalsButton");
 equals.addEventListener("click", ()=> {
     calculate();
