42af4cd9718807cb4fd23754b6c47cefed9136fe
Frederik Skrubbeltrang <80112882+fredeskrubba@users.noreply.github.com>
2021-10-18 13:27:23 +0200

    Add files via upload

--- Calculator/script.js
+++ Calculator/script.js
@@ -43,19 +43,31 @@ const numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const operatorArray = ["+", "-", "*", "/"];
 
 // create the functions that populate the display
-// Check if the button is a number or an operator, by checking if it's in the former arrays.
+// Check if the button is a number or an operator, by checking if it's in one of the former arrays.
 // if it is, update the object first, then update the display.
 function changeDisplay(newNum){
-    if (numberArray.includes(parseInt(newNum)) && operatorArray.includes(calculatorInfo.inputOperator)){
+    // check if there already is an operator in the object, and if an operator was pressed again.
+    // if true, calculate and show the new sum.
+    if (operatorArray.includes(calculatorInfo.inputOperator) && operatorArray.includes(newNum)){
+        calculate();
+        calculatorInfo.inputNumber1 = String(calculatorInfo.currentSum);
+        calculatorInfo.inputNumber2 = "";
+        calculatorInfo.inputOperator = newNum;2
+    // check if the operator has a value, if true, the button pressed goes to inputnumber2
+    } else if (numberArray.includes(parseInt(newNum)) && operatorArray.includes(calculatorInfo.inputOperator)){
         calculatorInfo.inputNumber2 += newNum;
+        calculatorInfo.currentSum = operate(calculatorInfo.inputOperator, parseInt(calculatorInfo.inputNumber1), parseInt(calculatorInfo.inputNumber2));
         displayNum.textContent = calculatorInfo.inputNumber2;
+    // Check if it's the first number pressed
     } else if (numberArray.includes(parseInt(newNum))){
         calculatorInfo.inputNumber1 += newNum;
+        calculatorInfo.currentSum = parseInt(calculatorInfo.inputNumber1);
         displayNum.textContent = calculatorInfo.inputNumber1;
-        
+    // Check if the button pressed is an operator.
+    // set inputnumber1 to currentSum, so you can use the sum after pressing equals.   
     } else if (operatorArray.includes(newNum)){
+        calculatorInfo.inputNumber1 = String(calculatorInfo.currentSum);
         calculatorInfo.inputOperator = newNum;
-        displayNum.textContent = calculatorInfo.inputOperator;
     };
 };
 
@@ -75,20 +87,29 @@ buttonArray.forEach(button => {
 // A function that calls the operate function when the = button is pressed.
 function calculate(){
     calculatorInfo.currentSum = operate(calculatorInfo.inputOperator, parseInt(calculatorInfo.inputNumber1), parseInt(calculatorInfo.inputNumber2));
-    return displayNum.textContent = calculatorInfo.currentSum;
-};
-
-// function that resets the calculatorInfo object so you can do math again.
-function reset(){
     calculatorInfo.inputNumber1 = "";
     calculatorInfo.inputNumber2 = "";
     calculatorInfo.inputOperator = "";
+    return displayNum.textContent = calculatorInfo.currentSum;
 };
 
 // Use the info from the calculatorInfo object to do math when = is pressed, then reset the info. 
 const equals = document.querySelector(".equalsButton");
-equals.addEventListener("click", ()=> {
+equals.addEventListener("click", () => {
     calculate();
+});
+
+// make a clear button.
+function reset(){
+    displayNum.textContent = "Display"
+    calculatorInfo.inputNumber1 = "";
+    calculatorInfo.inputNumber2 = "";
+    calculatorInfo.inputOperator = "";
+    calculatorInfo.currentSum = 0;
+};
+
+let clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", ()=>{
     reset();
 });
 
